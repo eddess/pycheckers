5edafd6204d5d8f9ede26fd5d56b4d952b172920
Eddy Essien <eddy.essien@gmail.com>
2014-08-31 09:27:49 +0100

    Lint checked

--- pycheckers.py
+++ pycheckers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-
-# ' extra apostrophe to fix Emacs python-mode broken string matching
+"""Combines pylint and pep8 for checking"""
 
 # Copyright (c) 2002-present, Damn Simple Solutions Ltd.
 # All rights reserved.
@@ -32,16 +31,19 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import re
+from __future__ import print_function
+from re import compile as re_compile
 from optparse import OptionParser
 import sys
 from StringIO import StringIO
+from os.path import exists as file_exists
 
 from pep8 import _main as pep8main
 from pylint import run_pylint as pylintmain
 
 
 class LintRunner(object):
+    """The base class for a lint object"""
     def __init__(self, debug=False):
         self._debug = debug
         self.sane_default_ignore_codes = set([])
@@ -49,46 +51,37 @@ class LintRunner(object):
         self.run_command = None
         self.env = None
         self._debug = debug
-        self.output_matcher = re.compile(
+        self.output_matcher = re_compile(
             r'^(([a-zA-Z]:){0,1}[^:]+):(\d+):(\d+): ([a-zA-Z]\d+) (.*)$')
 
-
     @property
-    def run_flags(self):
+    def run_flags(self):  # pylint: disable=R0201
+        """Return flags for running the linter"""
         return ()
 
-
     def process_line(self, line, filename):
-        m = self.output_matcher.match(line)
+        """Parse output line"""
+        line_m = self.output_matcher.match(line)
         output = ''
-        if m is not None:
-            output = filename + ':' + m.group(3) + ':'
-            if m.group(4) == 0:
-                output += '1: '
+        if line_m is not None:
+            column = int(line_m.group(4)) + 1
+            code = line_m.group(5)[0]
+            if code in ['C', 'E', 'R', 'W']:
+                output = filename + ':' + line_m.group(3) + ':' \
+                    + str(column) + ':' \
+                    + line_m.group(5)[0] + ':' \
+                    + '(' + line_m.group(5) + ') ' + line_m.group(6)
             else:
-                output += m.group(4) + ': '
+                return
+            print(output)
 
-            if m.group(5).startswith('E'):
-                output += 'error '
-            elif m.group(5).startswith('W'):
-                output += 'warning '
-            elif m.group(5).startswith('F'):
-                output += 'fatal '
-            elif m.group(5).startswith('C'):
-                output += 'convention '
-            else:
-                return()
-
-            output += '(' + m.group(5) + ') ' + m.group(6)
-            print (output)
-
-
-    def run(self, filename):
+    def run(self, filenames):
+        """Execute the linter"""
         args = [self.command]
         args.extend(self.run_flags)
-        args.extend(filename)
+        args.extend(filenames)
         if self._debug:
-            print "DEBUG: command = ", ' '.join(args)
+            print("DEBUG: command = ", ' '.join(args))
 
         sysargv = sys.argv
         sys.argv = args
@@ -99,8 +92,9 @@ class LintRunner(object):
         try:
             sys.stdout = new_stdout
             sys.stderr = new_stderr
-            self.run_command()
-        except (SystemExit):
+            if callable(self.run_command):
+                self.run_command()  # pylint: disable=E1102
+        except SystemExit:
             pass
         finally:
             sys.stdout = old_stdout
@@ -112,28 +106,30 @@ class LintRunner(object):
         new_stderr = new_stderr.getvalue().split('\n')
         for line in new_stdout:
             if self._debug:
-                print self.command, 'STDOUT:', line
-                print '\n\n'
-            self.process_line(line, filename[0])
+                print(self.command, 'STDOUT:', line)
+                print('\n\n')
+            self.process_line(line, filenames[0])
 
         for line in new_stderr:
             if self._debug:
-                print self.command, 'STDERR:', line
+                print(self.command, 'STDERR:', line)
 
 
 class PylintRunner(LintRunner):
+    """Linting with the pylint linter"""
 
     def __init__(self, debug=False):
-        super(PylintRunner, self).__init__(debug = debug)
+        super(PylintRunner, self).__init__(debug=debug)
         self.command = 'pylint'
         self.run_command = pylintmain
         self.sane_default_ignore_codes = set([
-            'F0401', # Unable to import
-            'E0611', # module does not contain symbol. Handled by Jedi
-            'E1123', # unexpected argument in constructor
-            'E1101', # does not have member. Jedi handles this
-            'W0232', # no init method in class
-            'E1120', # no constructor for
+            'F0401',  # Unable to import
+            'E0611',  # module does not contain symbol. Handled by Jedi
+            'E1123',  # unexpected argument in constructor
+            'E1101',  # does not have member. Jedi handles this
+            'W0232',  # no init method in class
+            'E1120',  # no constructor for
+            'C0330',  # wrong indentation handled by pep8
         ])
 
     @property
@@ -143,30 +139,25 @@ class PylintRunner(LintRunner):
                 '--disable=' + ','.join(self.sane_default_ignore_codes))
 
 
-
 class Pep8Runner(LintRunner):
+    """Linting with pep8"""
 
-    def __init__(self, debug = False):
-        super(Pep8Runner, self).__init__(debug = debug)
+    def __init__(self, debug=False):
+        super(Pep8Runner, self).__init__(debug=debug)
         self.command = 'pep8'
         self.run_command = pep8main
         self.sane_default_ignore_codes = set([
         ])
 
-
     @property
     def run_flags(self):
-        return ('--repeat'
-                , '--filename=*py'
-                , '--ignore=%s'%','.join(self.sane_default_ignore_codes))
+        return ('--repeat',
+                '--filename=*py',
+                '--ignore=%s' % ','.join(self.sane_default_ignore_codes))
 
-
-################################################################################
-checkers = {  Pep8Runner, PylintRunner, }
-
-################################################################################
 
 def main():
+    """Execution entry point"""
     usage = "usage: %prog [options] [PY_MODULE_OR_PACKAGE]..."
     parser = OptionParser(usage=usage)
 
@@ -175,14 +166,30 @@ def main():
         action="store_true",
         default=False,
         help=("show debug output"))
-    options, filename = parser.parse_args()
+    parser.add_option(
+        '-r', "--reports",
+        default='y',
+        help=("Just for flycheck's sake"))
+    parser.add_option(
+        "--msg-template",
+        help=("Just for flycheck's sake"))
+    options, filenames = parser.parse_args()
 
-    if len(filename) < 1:
-        exit(0)
+    if len(filenames) < 1:
+        parser.print_help()
+        exit(1)
 
-    for RunnerClass in checkers:
-        runner = RunnerClass( debug=options.debug)
-        runner.run(filename)
+    for filename in filenames:
+        if not file_exists(filename):
+            print('Error: ' + filename + ' does not exist')
+            exit(1)
+
+    for RunnerClass in {Pep8Runner, PylintRunner}:  # pylint: disable=C0103
+        runner = RunnerClass(debug=options.debug)
+        try:
+            runner.run(filenames)
+        except TypeError:
+            pass
 
 if __name__ == '__main__':
     main()
